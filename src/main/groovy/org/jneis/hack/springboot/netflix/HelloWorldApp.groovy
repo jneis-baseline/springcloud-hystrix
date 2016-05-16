25b2f0724448eae0875f21721b784a9095b3c1c9
Josue Neis <jneis@github.com>
2016-05-16 16:30:09 -0300

    add hystrix dashboard

--- src/main/groovy/org/jneis/hack/springboot/netflix/HelloWorldApp.groovy
+++ src/main/groovy/org/jneis/hack/springboot/netflix/HelloWorldApp.groovy
@@ -3,9 +3,11 @@ package org.jneis.hack.springboot.netflix
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 class HelloWorldApp {
 
     static void main(String... args) {
